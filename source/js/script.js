1e258d8b4fb589db4ee6fea377ea587d7716b6e0
Miuniverso <m.yana93@mail.ru>
2020-06-12 16:41:42 +0300

    Navigation + intro + facts

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,54 @@
+// 'use strict';
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.main-header__burger');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+console.log('1');
 
-pageHeader.classList.remove('page-header--nojs');
+// open burger menu
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
+var navButton = document.querySelector(".main-header__button");
+var menu = document.querySelector(".main-nav");
+var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+if (viewportWidth < 1023) {
+  console.log('<1024');
+  menu.classList.add("visually-hidden");
+  console.log('2');
+  navButton.classList.remove('visually-hidden');
+  console.log('3');
+} else {
+  menu.classList.remove("visually-hidden");
+  navButton.classList.add('visually-hidden');
+}
+
+window.addEventListener("resize", function() {
+  viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  if (viewportWidth > 1023) {
+    menu.classList.remove("visually-hidden");
+    navButton.classList.add('visually-hidden');
   } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+    menu.classList.add("visually-hidden");
+    navButton.classList.remove('visually-hidden');
   }
+}, false);
+
+navButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  menu.classList.toggle('visually-hidden');
+  navButton.classList.toggle('main-header__button--active');
 });
